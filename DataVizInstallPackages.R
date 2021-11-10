8b9788c2d29d4013fbfc8ee293f19f5eda3795ff
Goh Rui Kun <61505106+rkapril@users.noreply.github.com>
2021-11-10 23:54:45 +0800

    Add files via upload

---
+++ DataVizInstallPackages.R
@@ -0,0 +1,13 @@
+install.packages("tidyverse")
+install.packages("plotly")
+install.packages("highcharter")
+install.packages("leaflet")
+install.packages("igraph")
+install.packages("visNetwork")
+install.packages("DT")
+install.packages("rmarkdown")
+install.packages("flexdashboard")
+install.packages("shiny")
+install.packages("forecast")
+
+
